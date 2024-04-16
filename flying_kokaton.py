09bddafba1a822345a28b491a1fd56502ee7a73e
kota <c0b23048bf@edu.teu.ac.jp>
2024-04-16 17:26:49 +0900

    もろもろ変更

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -6,35 +6,41 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
+    #スクリーン設定
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    
+    #背景画像とこうかとん
     bg_img = pg.image.load("fig/pg_bg.jpg")
     c_img = pg.image.load("fig/3.png")
+    
+    #こうかとん座標設定
     c_img = pg.transform.flip(c_img, True, False)
     c_img_rct = c_img.get_rect()
     c_img_rct.center = 300,200
-    tmr = 0
-    bg_x = 0
-    bg_y = 0
-    bg_ax = 1600
-    bg_ay = 0
-    c_x = 0
-    c_y = 0
+    
+    tmr,bg_x,bg_y,bg_ax,bg_ay,c_x ,c_y = 0,0,0,1600,0,0,0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        #背景画像blit
         screen.blit(bg_img, [bg_x, bg_y])
         screen.blit(bg_img,[bg_ax,bg_ay])
+        
         tmr += 1
         bg_x -= 1
         bg_ax -= 1
-        c_x = 0
-        c_y = 0
+        c_x,c_y = 0,0
+        
+        #背景ループ
         if bg_x <= -1600:
             bg_x = 0
             bg_ax = 1600
+        
+        #こうかとんボタン操作
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             c_y += -1
@@ -48,7 +54,7 @@ def main():
             c_x += -1
         c_img_rct.move_ip((c_x,c_y))
         screen.blit(c_img,c_img_rct)
-        #bg_y = 0        
+            
         clock.tick(200)
         pg.display.update()
 
